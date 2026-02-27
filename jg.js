62d9cfd5d05a98dd671035212982ac46036c7db6
Jgziin <jorgehenrique.miranda20@gmail.com>
2026-02-27 17:58:03 -0300

    update
    .

---
+++ jg.js
@@ -0,0 +1,22 @@
+
+let numero5= 10;
+let numero10= 20;
+let resultado= numero5 + numero10;
+console.log(numero10 + numero5);
+
+let produto = "camisa";
+let preço = 10.00;
+console.log (`o valor desta ${produto} está custando ${preço} reais. `);
+
+let usuario = "Jorge";
+const id = 237;
+console.log(`Usuario: ${usuario}
+id: ${id}`);
+
+
+
+
+
+
+
+
